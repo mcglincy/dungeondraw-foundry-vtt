9d8d4ed5909176d8e2eca489dc21718ea7e65f10
mcglincy <mattmcglincy@gmail.com>
2021-12-05 07:58:14 -0600

    Generate sourcemap and remove commented-out code.

--- rollup.config.js
+++ rollup.config.js
@@ -12,61 +12,11 @@ export default () => {
         format: "es",
         plugins: [ terser() ],
         preferConst: true,
+        sourcemap: true,
       },
       plugins: [
         nodeResolve(),
       ],
     },
-    // {
-    //   input: "pack",
-    //   output: [
-    //     {
-    //       file: "dist/jsts.js",
-    //       format: "es",
-    //       plugins: [],
-    //       preferConst: true,
-    //       //sourcemap: s_SOURCEMAP,
-    //     },
-    //   ],
-    //   plugins: [
-    //     virtual({
-    //       pack: `import jsts from './node_modules/jsts/dist/jsts.js';`,
-    //     }),
-    //   ],
-    // },    
-    // {
-    //   input: "pack",
-    //   output: [
-    //     {
-    //       file: "dist/pixi-filter-alpha.js",
-    //       format: "es",
-    //       plugins: [],
-    //       preferConst: true,
-    //       //sourcemap: s_SOURCEMAP,
-    //     },
-    //   ],
-    //   plugins: [
-    //     virtual({
-    //       pack: `import { AlphaFilter, BlurFilterPass } from './node_modules/@pixi/filter-alpha/dist/esm/filter-alpha.js';`,
-    //     }),
-    //   ],
-    // },
-    // {
-    //   input: "pack",
-    //   output: [
-    //     {
-    //       file: "dist/pixi-filter-blur.js",
-    //       format: "es",
-    //       plugins: [],
-    //       preferConst: true,
-    //       //sourcemap: s_SOURCEMAP,
-    //     },
-    //   ],
-    //   plugins: [
-    //     virtual({
-    //       pack: `import { BlurFilter, BlurFilterPass } from './node_modules/@pixi/filter-blur/dist/esm/filter-blur.js';`,
-    //     }),
-    //   ],
-    // },
   ];
 };
